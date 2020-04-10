16bb008d9f96e091393ac75810cfe0e57b59a825
Brian Wheeler <brianwheeler@Brians-MacBook-Pro.local>
2020-04-10 10:26:46 -0700

    test the edge() function of the base opton token contract

--- test/scripts/edge-test.js
+++ test/scripts/edge-test.js
@@ -5,18 +5,13 @@ const DefaultBalanceToken = artifacts.require('DefaultBalanceToken')
 const { fromWei } = web3.utils
 
 contract('Option Token', accounts => {
-    let dsfToken, call, put, zeroBalance, edgeBalance
+    let call, put, zeroBalance, edgeBalance
 
     before(async () => {
         edgeBalance = accounts[0]
         zeroBalance = accounts[9]
-        dsfToken = await DefaultBalanceToken.deployed()
         call = await ETHCallOptionToken.deployed()
         put = await ETHPutOptionToken.deployed()
-
-        // burn zero balance trader's default balance
-        const balance = await dsfToken.balanceOf(zeroBalance)
-        await dsfToken.transfer('0x0000000000000000000000000000000000000000', balance, { from: zeroBalance })
     })
 
     it('returns the edge of a trader with DefaultBalance', async () => {

c87b2bb6d0136ea6a0adf5e12a047223f8ea74d6
Brian Wheeler <brianwheeler@Brians-MacBook-Pro.local>
2020-06-25 11:18:38 -0700

    truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -20,11 +20,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 9545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
 
     // Another network with more advanced options...
     // advanced: {
@@ -57,7 +57,7 @@ module.exports = {
       network_id: 42,
       skipDryRun: true,
       gasPrice: 1e9
-    }
+    },
     // Useful for private networks
     // private: {
       // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
